d2623957ac876f681f26aa41328f604b3ae90169
Boris Joskic <boris.joskic@jsguru.io>
2019-04-10 19:24:08 +0200

    Sidemenu web component

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { initSidemenu } from "./src/components/Sidemenu/Sidemenu";
 import initHome from "./src/components/Home/Home";
-
+import './src/components/Sidemenu/SidemenuTemplate';
 import("./app.scss");
 
 window.onload = () => {
--- src/components/Sidemenu/Sidemenu.js
+++ src/components/Sidemenu/Sidemenu.js
@@ -1,34 +1,4 @@
-import { MDCList } from '@material/list';
-
-const sitemenuTemplate = () => `
-    <aside class="mdc-drawer">
-      <div class="mdc-drawer__content">
-        <nav class="mdc-list">
-          <a
-            class="mdc-list-item mdc-list-item--activated"
-            href="#"
-            aria-current="page"
-          >
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true"
-              >inbox</i
-            >
-            <span class="mdc-list-item__text">Inbox</span>
-          </a>
-          <a class="mdc-list-item" href="#">
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true"
-              >send</i
-            >
-            <span class="mdc-list-item__text">Outgoing</span>
-          </a>
-          <a class="mdc-list-item" href="#">
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true"
-              >drafts</i
-            >
-            <span class="mdc-list-item__text">Drafts</span>
-          </a>
-        </nav>
-      </div>
-    </aside>`;
+const sitemenuTemplate = () => '<sidemenu-nav></sidemenu-nav>';
 
 const initSidemenu = () => {
   const app = document.querySelector('#app');
@@ -38,9 +8,6 @@ const initSidemenu = () => {
   sidemenu.innerHTML = sitemenuTemplate();
 
   app.appendChild(sidemenu);
-
-  const list = MDCList.attachTo(document.querySelector('.mdc-list'));
-  list.wrapFocus = true;
 };
 
 const openSidemenu = () => {
